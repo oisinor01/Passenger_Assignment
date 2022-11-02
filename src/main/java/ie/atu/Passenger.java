cbe3088b57735ddcee6251be205ca61247e1d7d9
oisinor01 <g00379850@atu.ie>
2022-11-02 20:32:12 +0000

    commiting with tests

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -1,5 +1,7 @@
 package ie.atu;
 
 public class Passenger {
+    int counter;
+
 
 }
---
+++ src/test/java/ie/atu/PassengerTest.java
@@ -0,0 +1,43 @@
+package ie.atu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassengerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testTitle(){
+
+    }
+
+    @Test
+    void testName(){
+
+    }
+
+    @Test
+    void testID(){
+
+    }
+
+    @Test
+    void testPhone(){
+
+    }
+
+    @Test
+    void testAge(){
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
